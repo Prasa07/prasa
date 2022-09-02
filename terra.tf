043c6831d327adadf2d782d5d6ba64ee029ba17a
Prasa07 <96566339+Prasa07@users.noreply.github.com>
2022-09-02 20:21:32 +0530

    Delete terra.tf

--- terra.tf
+++
@@ -1,9 +0,0 @@
-provider "aws"{
- access_key="AKIAZGO5BBLN5BM3UE4M"
- secret_key="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"
- region="ap-south-1"
-}
-resource "aws_instance" "terraformvijay"{
-ami="ami-0460ce78b2cf43489"
-instance_type="t2.micro"
-}
